743a8a3a6f75779382dda7cb7b1ebbddc65543c3
Emilie Z <68432655+EmilieYZhang@users.noreply.github.com>
2022-06-18 23:56:12 -0400

    Update redeploy-site.sh
    Automate redeployment in VPS with tmux. 'redeploy-site.sh' is stored in directory above personal-portfolio.

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -12,4 +12,4 @@ pip install -r requirements.txt
 
 echo #all the environment variables >> .env
 
-tmux new-session -d -s my_session 'cd personal-portfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+tmux new-session -d -s my_session 'flask run --host=0.0.0.0'
