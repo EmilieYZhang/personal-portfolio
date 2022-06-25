51247ab53f81c2712bf85d9cec732180605be245
EmilieYZhang <emilieyzh@gmail.com>
2022-06-25 13:56:38 -0400

    updated redeploy-site.sh using services

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-tmux kill-server
-
 cd personal-portfolio
 
 git fetch && git reset origin/main --hard
@@ -12,4 +10,4 @@ pip install -r requirements.txt
 
 echo #all the environment variables >> .env
 
-tmux new-session -d -s my_session 'flask run --host=0.0.0.0'
+systemctl restart myportfolio
