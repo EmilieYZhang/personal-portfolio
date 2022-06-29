e138a453d6d442ef1907837245b8a17e111eb73f
EmilieYZhang <emilieyzh@gmail.com>
2022-06-29 12:47:07 -0400

    Added delete endpoint in curl test

--- curl-test.sh
+++ curl-test.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-#post request
+# post request
 curl --request POST http://localhost:5000/api/timeline_post -d 'name=Emilie&email=emilieyzh@gmail.com&content=Adding a database from bash script to my portfolio site!'
 
-#test post request by checking with GET
+# test post request by checking with GET
 curl http://localhost:5000/api/timeline_post
+
+# test delete request with DELETE endpoint
+# curl -X DELETE http://localhost:5000/api/timeline_post/<id>
